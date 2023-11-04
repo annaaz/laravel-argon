93a650ed04d693dd201f27c3a1bbbee3e3def807
Mochammad Annaas Annaafi <mochammad.annaas.annaafi@mncgroup.com>
2023-11-04 19:32:40 +0700

    feat(sisma-unhan): Headline and footer

--- resources/views/layouts/footers/guest/footer.blade.php
+++ resources/views/layouts/footers/guest/footer.blade.php
@@ -2,7 +2,9 @@
     <div class="text-white position-relative">
             SISMA | SISTEM INFORMASI KERJASAMA <br />
             <span>UNIVERSITAS PERTAHANAN RI</span> <br />
-        Kawasan IPSC Sentul, Bogor, Jawa Barat<br /><br />
+        Kawasan IPSC Sentul, Kabupaten Bogor, Jawa Barat, 16810 (Kampus Utama)<br />
+        Jl. Salemba Raya No.14 Jakarta Pusat, DKI Jakarta 10430 (Kampus Satelit) <br />
+        Fatuketi, Kab Belu, Nusa Tenggara Timur 85752 (Kampus Satelit) <br /><br />
         KONTAK INFO <br /><br /><br />
             <small>
             No Telp : +6221-87951555 / 87954555  <br />
